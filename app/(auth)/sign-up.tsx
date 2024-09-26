1abba18ceb3de69f624415dacc938042eac906af
RuslanFom <ruslanfom@gmail.com>
2024-09-26 14:16:15 +0200

    implement Auth screens

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -4,6 +4,7 @@ import InputField from "@/components/InputField";
 import { useState } from "react";
 import CustomButton from "@/components/CustomButton";
 import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -54,7 +55,8 @@ const SignUp = () => {
             className="mt-6"
           />
 
-          {/*OAuth*/}
+          <OAuth />
+
           <Link
             href="/sign-in"
             className="text-lg text-center text-general-200 mt-10"
@@ -63,7 +65,7 @@ const SignUp = () => {
             <Text className="text-primary-500">Log In</Text>
           </Link>
         </View>
-        {/*Verification*/}
+        {/*Verification Modal*/}
       </View>
     </ScrollView>
   );
